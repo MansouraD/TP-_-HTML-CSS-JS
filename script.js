43a97e799ee85dc69102a3180da9f8bb193629dc
Mariam D <62890032+MansouraD@users.noreply.github.com>
2020-06-24 13:03:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+function afficherForm (){
+    document.getElementById("info_client").style.display="block"
+    document.getElementById("info_compte").style.display="block"
+    document.getElementById("info_employeur").style.display="block"
+    document.getElementById("soumettre").style.display="block"
+}
